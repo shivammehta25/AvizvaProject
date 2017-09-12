73f564fae639c7382928f557b99cd3e3c3d03b3f
Shivam Mehta <shivam.mehta007@gmail.com>
2017-09-12 11:04:39 +0530

    Created Maven BackEnd Project

---
+++ BackEnd/src/main/java/com/avizva/trainingProject/backend/App.java
@@ -0,0 +1,13 @@
+package com.avizva.trainingProject.backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
