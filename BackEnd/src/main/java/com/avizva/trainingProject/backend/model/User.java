2afa139a7069a933c9840dc768ef834fbbf8acc9
Shivam Mehta <shivam.mehta007@gmail.com>
2017-09-12 13:13:47 +0530

    Git ingore Trying in all folders

--- BackEnd/src/main/java/com/avizva/trainingProject/backend/model/User.java
+++ BackEnd/src/main/java/com/avizva/trainingProject/backend/model/User.java
@@ -1,7 +1,85 @@
 package com.avizva.trainingProject.backend.model;
 
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 
+	private String name;
+
+	private String email;
+
+	private Long contact;
+
+	private Date dob;
+
+	private String role;
+
+	private boolean enable;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getContact() {
+		return contact;
+	}
+
+	public void setContact(Long contact) {
+		this.contact = contact;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public boolean isEnable() {
+		return enable;
+	}
+
+	public void setEnable(boolean enable) {
+		this.enable = enable;
+	}
+	
+	
 }
