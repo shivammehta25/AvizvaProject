f1e1184eaf04be0557fcd03fa0b257de2a27baca
Shivam Mehta <shivam.mehta007@gmail.com>
2017-09-13 17:07:07 +0530

    Update ApplicationContextConfig.java

--- BackEnd/src/main/java/com/avizva/trainingProject/backend/config/ApplicationContextConfig.java
+++ BackEnd/src/main/java/com/avizva/trainingProject/backend/config/ApplicationContextConfig.java
@@ -32,7 +32,7 @@ public class ApplicationContextConfig {
 	   dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 	    dataSource.setUrl("jdbc:mysql://localhost:3306/project");
 	    dataSource.setUsername("root"); 
-	    dataSource.setPassword("");
+	    dataSource.setPassword("toor");
 
 	    return dataSource;
 	}
