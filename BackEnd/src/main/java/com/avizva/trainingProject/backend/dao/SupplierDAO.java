2d0fc6182db0c64c73737458348c23d1021ac482
saurabhmavizva <saurabh.miglani@avizva.com>
2017-09-16 14:15:45 +0530

    Pulled SupplierDAO+ Merged Conflicts Again

--- BackEnd/src/main/java/com/avizva/trainingProject/backend/dao/SupplierDAO.java
+++ BackEnd/src/main/java/com/avizva/trainingProject/backend/dao/SupplierDAO.java
@@ -9,12 +9,7 @@ public interface SupplierDAO {
 	public boolean createSupplier(Supplier supplier);
 	public Supplier getSupplierById(int id);
 	public boolean updateSupplier(Supplier supplier);
-<<<<<<< HEAD
-	public boolean deleteSupplier(Supplier supplier);
-	public List<Supplier> getallSupplier(Supplier supplier);
-	
-=======
+
 	public boolean deactivateSupplier(Supplier supplier);
 	public List<Supplier> getallSupplier();
->>>>>>> 648fa0194027fe28dd99ee2869aa9e79fce2ff9d
  }
