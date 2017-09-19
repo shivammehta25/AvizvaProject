85c9f049882d49721a30d0e1c897998d957a0353
Shivam Mehta <shivam.mehta007@gmail.com>
2017-09-19 09:59:53 +0530

    Fixed Master Branch

--- BackEnd/src/main/java/com/avizva/trainingProject/backend/dao/UserDAO.java
+++
@@ -1,8 +0,0 @@
-package com.avizva.trainingProject.backend.dao;
-
-import com.avizva.trainingProject.backend.model.User;
-
-public interface UserDAO {
-	public boolean registerUser(User user);
-	
-}
